46070e8e24bc15c873f783f6e6d4e03be33b306a
Bender11011 <nezarsanad@outlook.com>
2023-04-16 11:39:32 +0200

    Initial Commit

---
+++ Mod7b_project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Mod7b_project
+//
+//  Created by Nezar Sanad on 16/04/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
